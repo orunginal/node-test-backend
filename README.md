bfdad125f930c171d69f0fe489531ee77b834b46
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-03-25 22:42:09 +0100

    Allow cross origin & adapt to new syntaxt with callbacks

--- README.md
+++ README.md
@@ -4,47 +4,6 @@ This is a NodeJS API that supports username and password authentication with JWT
 
 ## Available APIs
 
-### User APIs
-
-#### POST `/users`
-
-You can do a POST to `/users` to create a new user.
-
-The body must have:
-
-* `username`: The username
-* `password`: The password
-* `extra`: Some extra information you want to save from the user (It's a string). This could be a color or anything at all.
-
-It returns the following:
-
-```json
-{
-  "id_token": {jwt}
-}
-```
-
-The JWT is signed with the secret located at the `config.json` file. That JWT will contain the `username` and the `extra` information sent.
-
-#### POST `/sessions/create`
-
-You can do a POST to `/sessions/create` to log a user in.
-
-The body must have:
-
-* `username`: The username
-* `password`: The password
-
-It returns the following:
-
-```json
-{
-  "id_token": {jwt}
-}
-```
-
-The JWT is signed with the secret located at the `config.json` file. That JWT will contain the `username` and the `extra` information that you sent at signup time.
-
 ### Quotes API
 
 #### GET `/api/random-quote`
--- user-routes.js
+++ user-routes.js
@@ -5,11 +5,14 @@ var express = require('express'),
 
 var app = module.exports = express.Router();
 
+
+
+
 // XXX: This should be a database of users :).
 var users = [{
   id: 1,
-  username: 'gonto',
-  password: 'gonto'
+  username: 'test@test.com',
+  password: 'password'
 }];
 
 function createToken(user) {
@@ -21,18 +24,18 @@ function getUserScheme(req) {
   var username;
   var type;
   var userSearch = {};
+  //var test = JSON.parse(req.query);
+  var str = '{ "name": "John Doe", "age": 42 }';
+  var obj = JSON.parse(str);
+  console.log(req.query);
+  console.log(req.query.callback);
+  console.log(req.query.user);
 
   // The POST contains a username and not an email
-  if(req.body.username) {
-    username = req.body.username;
+  if(req.query.user.email) {
+    username = req.query.user.email;
     type = 'username';
     userSearch = { username: username };
-  }
-  // The POST contains an email and not an username
-  else if(req.body.email) {
-    username = req.body.email;
-    type = 'email';
-    userSearch = { email: username };
   }
 
   return {
@@ -42,16 +45,17 @@ function getUserScheme(req) {
   }
 }
 
+
 app.post('/users', function(req, res) {
-  
+
   var userScheme = getUserScheme(req);  
 
-  if (!userScheme.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
+  if (!userScheme.username || !req.body.user.password) {
+    return res.status(400).jsonp("You must send the username and the password");
   }
 
   if (_.find(users, userScheme.userSearch)) {
-   return res.status(400).send("A user with that username already exists");
+   return res.status(400).jsonp("A user with that username already exists");
   }
 
   var profile = _.pick(req.body, userScheme.type, 'password', 'extra');
@@ -59,30 +63,30 @@ app.post('/users', function(req, res) {
 
   users.push(profile);
 
-  res.status(201).send({
-    id_token: createToken(profile)
+  res.status(201).jsonp({
+    token: createToken(profile)
   });
 });
 
-app.post('/sessions/create', function(req, res) {
+app.get('/sessions/create', function(req, res) {
 
   var userScheme = getUserScheme(req);
 
-  if (!userScheme.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
+  if (!userScheme.username || !req.query.user.password) {
+    return res.status(400).jsonp("You must send the username and the password");
   }
 
   var user = _.find(users, userScheme.userSearch);
   
   if (!user) {
-    return res.status(401).send("The username or password don't match");
+    return res.status(401).jsonp("The username or password don't match");
   }
 
-  if (user.password !== req.body.password) {
-    return res.status(401).send("The username or password don't match");
+  if (user.password !== req.query.user.password) {
+    return res.status(401).jsonp("The username or password don't match");
   }
 
-  res.status(201).send({
-    id_token: createToken(user)
+  res.status(201).jsonp({
+    token: createToken(user)
   });
 });
